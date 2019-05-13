7871f25032b0767390a706f50d84ff1ac398f793
WQ <36372552+WQmmm@users.noreply.github.com>
2019-05-13 19:24:10 +0800

    Create Cstack.h

---
+++ day2/顺序栈/Cstack.h
@@ -0,0 +1,21 @@
+/////Cstack.h/////
+
+#define EMPTY -1
+typedef int op_code;
+
+typedef int element_type;
+const int M=10;
+
+class Cstack
+{
+private:
+	int top;
+	element_type s[M];
+public:
+	Cstack();
+	bool empty()const;
+	op_code pop(element_type &x);      //退栈函数声明
+	op_code push();                    //进栈函数声明
+	int display();                     //查看栈中元素个数函数声明
+	int show();
+};
